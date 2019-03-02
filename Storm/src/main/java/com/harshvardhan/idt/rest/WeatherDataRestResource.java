0a1ef9f511dd286fd802b90188d547fc6f3c5f90
Harshvardhan Dudeja <harshvardhandudeja@HarshvadhansAir.fios-router.home>
2019-03-02 14:55:40 -0500

    AutoDetect Location from IP

--- Storm/src/main/java/com/harshvardhan/idt/rest/WeatherDataRestResource.java
+++ Storm/src/main/java/com/harshvardhan/idt/rest/WeatherDataRestResource.java
@@ -1,10 +1,13 @@
 package com.harshvardhan.idt.rest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.harshvardhan.idt.service.WeatherService;
+import com.harshvardhan.idt.util.IPUtil;
 
 import io.swagger.annotations.Api;
 
@@ -25,9 +29,24 @@ public class WeatherDataRestResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response get(@DefaultValue("new%20york") @QueryParam("city") String city) {
+	public Response get(@DefaultValue("new%20york") @QueryParam("city") String city, HttpServletRequest request) {
 		try {
+			System.out.println(request.getRemoteAddr());
 			return Response.ok(weatherService.getCurrentWeatherAndForecast(city.replaceAll(" ", "%20"))).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(Status.NOT_FOUND).entity(e.getMessage()).build();
+		}
+	}
+
+	@Path("/ip")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getIP(@Context HttpServletRequest request, @Context HttpServletResponse response) {
+		try {
+			String ip = IPUtil.getClientIpAddress(request);
+			System.out.println("IP:  " + ip);
+			return Response.ok(IPUtil.getLocationFromIp(ip)).build();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(Status.NOT_FOUND).entity(e.getMessage()).build();
---
+++ Storm/src/main/java/com/harshvardhan/idt/util/IPUtil.java
@@ -0,0 +1,42 @@
+package com.harshvardhan.idt.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.StringTokenizer;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+
+public class IPUtil {
+	public static String getClientIpAddress(HttpServletRequest request) {
+		String xForwardedForHeader = request.getHeader("X-Forwarded-For");
+		if (xForwardedForHeader == null) {
+			return request.getRemoteAddr();
+		} else {
+			// As of https://en.wikipedia.org/wiki/X-Forwarded-For
+			// The general format of the field is: X-Forwarded-For: client, proxy1, proxy2
+			// ...
+			// we only want the client
+			return new StringTokenizer(xForwardedForHeader, ",").nextToken().trim();
+		}
+	}
+
+	public static String getLocationFromIp(String ip) throws IOException, GeoIp2Exception {
+		File database = new File(IPUtil.class.getClassLoader().getResource("GeoLite2-City.mmdb").getPath());
+		DatabaseReader dbReader = new DatabaseReader.Builder(database).build();
+
+		InetAddress ipAddress = InetAddress.getByName(ip);
+		CityResponse response = dbReader.city(ipAddress);
+
+		String cityName = response.getCity().getName();
+		return cityName;
+	}
+
+	public static String getLocationFromIp(HttpServletRequest request) throws IOException, GeoIp2Exception {
+		return getLocationFromIp(getClientIpAddress(request));
+	}
+}
